02c9e480d74d73aace05dc3f44a35072652ccb4b
antonprihodko <antonprihodko@192.168.4.106>
2022-07-11 22:15:01 +0400

    Main class

---
+++ task10/src/main/java/com/foxminded/aprihodko/task10/UniversityApplication.java
@@ -0,0 +1,12 @@
+package com.foxminded.aprihodko.task10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityApplication.class, args);
+    }
+}
