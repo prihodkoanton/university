562dc01f58c75f9c18304d75d309fd36d7c03922
antonprihodko <antonprihodko@192.168.58.165>
2022-07-14 21:28:57 +0400

    Refactor code

--- task10/src/main/java/com/foxminded/aprihodko/task10/dao/AbstractCrudDao.java
+++ task10/src/main/java/com/foxminded/aprihodko/task10/dao/AbstractCrudDao.java
@@ -1,17 +1,16 @@
 package com.foxminded.aprihodko.task10.dao;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.foxminded.aprihodko.task10.models.Entity;
 
 public abstract class AbstractCrudDao<T extends Entity<K>, K> implements CrudDao<T, K> {
 
-    public T save(Connection connection, T entity) throws SQLException {
-        return entity.getId() == null ? create(connection, entity) : update(connection, entity);
+    public T save(T entity) throws SQLException {
+        return entity.getId() == null ? create(entity) : update(entity);
     }
 
-    protected abstract T create(Connection connection, T entity) throws SQLException;
+    protected abstract T create(T entity) throws SQLException;
 
-    protected abstract T update(Connection connection, T entity) throws SQLException;
+    protected abstract T update(T entity) throws SQLException;
 }
--- task10/src/main/java/com/foxminded/aprihodko/task10/dao/CrudDao.java
+++ task10/src/main/java/com/foxminded/aprihodko/task10/dao/CrudDao.java
@@ -1,6 +1,5 @@
 package com.foxminded.aprihodko.task10.dao;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -9,11 +8,11 @@ import com.foxminded.aprihodko.task10.models.Entity;
 
 public interface CrudDao<T extends Entity<K>, K> {
 
-    Optional<T> findById(Connection connection, K id) throws SQLException;
+    Optional<T> findById(K id) throws SQLException;
 
-    List<T> findAll(Connection connection) throws SQLException;
+    List<T> findAll() throws SQLException;
 
-    T save(Connection connection, T enity) throws SQLException;
+    T save(T enity) throws SQLException;
 
-    void deleteById(Connection connection, K id) throws SQLException;
+    void deleteById(K id) throws SQLException;
 }
