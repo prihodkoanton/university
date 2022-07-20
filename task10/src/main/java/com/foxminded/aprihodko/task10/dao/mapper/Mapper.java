41465fb61ca8c8110c24a89b1b3d514ed292239a
Anton Prihodko <tohathebest97@gmail.com>
2022-07-20 15:28:05 +0000

    Delete Mapper.java

--- task10/src/main/java/com/foxminded/aprihodko/task10/dao/mapper/Mapper.java
+++
@@ -1,9 +0,0 @@
-package com.foxminded.aprihodko.task10.dao.mapper;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface Mapper<T> {
-
-    T apply(ResultSet rs) throws SQLException;
-}
