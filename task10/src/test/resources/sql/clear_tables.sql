ae894d59553a32350a57297bd4195ca8580a5d2c
Anton Prihodko <tohathebest97@gmail.com>
2022-07-21 21:07:00 +0000

    Delete clear_tables.sql

--- task10/src/test/resources/sql/clear_tables.sql
+++
@@ -1,8 +0,0 @@
-delete from university.lessons cascade;
-delete from university.rooms cascade;
-delete from university.students cascade;
-delete from university.teachers cascade;
-delete from university.groups cascade;
-delete from university.courses cascade;
-delete from university.users cascade;
-
