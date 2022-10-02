01fa58dbc8b20095d596b3a190058e8c31607058
antonprihodko <antonprihodko@192.168.4.175>
2022-10-02 13:56:38 +0400

    Refactore code for security

--- task10/src/main/java/com/foxminded/aprihodko/task10/security/configuration/SecSecurityConfig.java
+++ task10/src/main/java/com/foxminded/aprihodko/task10/security/configuration/SecSecurityConfig.java
@@ -1,17 +1,14 @@
 package com.foxminded.aprihodko.task10.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -22,11 +19,11 @@ import com.foxminded.aprihodko.task10.services.impl.UserServiceImpl;
 @EnableWebSecurity
 public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
+    private final UserServiceImpl userDetailsService;
 
     @Autowired
-    public SecSecurityConfig(@Qualifier("userServiceImpl") UserServiceImpl userServiceImpl) {
-        this.userDetailsService = userServiceImpl;
+    public SecSecurityConfig(/* @Qualifier("userServiceImpl") */ UserServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -40,8 +37,8 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(daoAuthenticationProvider());
-        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(daoAuthenticationProvider());
+//        auth.userDetailsService(userDetailsService);
     }
 
 //    @Bean
@@ -50,7 +47,6 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 
     @Bean
-    @Primary
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -69,7 +65,7 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
     }
